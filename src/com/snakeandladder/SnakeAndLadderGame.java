5f37376d70d54e22a6f007219a5f425fab971f21
nikita <amarnikita09@gmail.com>
2021-12-09 12:52:46 +0530

    Refactored SnakeAndLadder.java

--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -5,6 +5,11 @@ public class SnakeAndLadderGame {
 	public static void main(String[] args) {
 		Person person1 = new Person();
 		System.out.println(person1.position);
+		
+		//die rolling
+		
+		int diceValue = (int)(Math.floor(Math.random()*10)%6+1);
+		System.out.println(diceValue);
 	}
 
 }
