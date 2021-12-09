0a0dcfa3d52ed903c426c48871e8c6906a24f8b3
nikita <amarnikita09@gmail.com>
2021-12-09 13:23:52 +0530

    Refactored SnakeAndLadder.java to make player reach Winning Position

--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -8,29 +8,35 @@ public class SnakeAndLadderGame {
 		Person person1 = new Person();
 		System.out.println(person1.position);
 		
-		//die rolling
-		
-		int diceValue = (int)(Math.floor(Math.random()*10)%6+1);
-		System.out.println(diceValue);
-		
-		//check options
-		int option = (int)(Math.floor(Math.random()*10)%3);
-		System.out.println(option);
-		
-		switch (option) {
-		case NO_PLAY:{
+		while(person1.position<=100) {
+			System.out.println("Rolling the Die");
+			//die rolling
+			int diceValue = (int)(Math.floor(Math.random()*10)%6+1);
+			System.out.println("Die Value came as: " +diceValue);
 			
-			break;
-		  }
-		case LADDER:{
-			person1.position = person1.position + diceValue;
-			break;
-		  }
-		case SNAKE:{
-			person1.position = person1.position - diceValue;
-			break;
-		  }
+			//check options
+			int option = (int)(Math.floor(Math.random()*10)%3);
+			System.out.println("player got a: " +option);
+			
+			switch (option) {
+			case NO_PLAY:{
+				
+				break;
+			  }
+			case LADDER:{
+				person1.position = person1.position + diceValue;
+				break;
+			  }
+			case SNAKE:{
+				person1.position = person1.position - diceValue;
+				if(person1.position<0)
+					person1.position = 0;
+				break;
+			  }
+			}
+			System.out.println("position is  "+ person1.position);
+			System.out.println("______________________");
 		}
-		System.out.println(person1.position);
+		
 	}
 }
