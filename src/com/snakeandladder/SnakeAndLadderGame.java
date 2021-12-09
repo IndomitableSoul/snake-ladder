60fdc61b283204a3b049134ff2fb91160b5b4f51
nikita <amarnikita09@gmail.com>
2021-12-09 12:44:43 +0530

    Adding SnakeLadder UC1 to master

---
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderGame {
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		System.out.println(person1.position);
+	}
+
+}
