93095e301a30c59f77c37aadb1836066170af9a6
nikita <amarnikita09@gmail.com>
2021-12-09 13:07:32 +0530

    Refactored SnakeAndLadder.java to check options

--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -1,7 +1,9 @@
 package com.snakeandladder;
 
 public class SnakeAndLadderGame {
-
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
 	public static void main(String[] args) {
 		Person person1 = new Person();
 		System.out.println(person1.position);
@@ -10,6 +12,25 @@ public class SnakeAndLadderGame {
 		
 		int diceValue = (int)(Math.floor(Math.random()*10)%6+1);
 		System.out.println(diceValue);
+		
+		//check options
+		int option = (int)(Math.floor(Math.random()*10)%3);
+		System.out.println(option);
+		
+		switch (option) {
+		case NO_PLAY:{
+			
+			break;
+		  }
+		case LADDER:{
+			person1.position = person1.position + diceValue;
+			break;
+		  }
+		case SNAKE:{
+			person1.position = person1.position - diceValue;
+			break;
+		  }
+		}
+		System.out.println(person1.position);
 	}
-
 }
