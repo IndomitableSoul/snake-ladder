8aa063bb3278e74c91aefd361221b35e89c8536b
nikita <amarnikita09@gmail.com>
2021-12-11 11:21:51 +0530

    Refactored SnakeAndLadder.java to UC7

--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -4,11 +4,9 @@ public class SnakeAndLadderGame {
     static final int NO_PLAY = 0;
     static final int LADDER = 1;
     static final int SNAKE = 2;
-	public static void main(String[] args) {
-		Person person1 = new Person();
-		System.out.println(person1.position);
-		int dieRollCount = 0;
-		while(person1.position<=100) {
+    public static int rollingTheDie(Person person1) {
+    	int dieRollCount = 0;
+		while(person1.position!=100) {
 			System.out.println("Rolling the Die");
 			dieRollCount++;
 			//die rolling
@@ -26,13 +24,13 @@ public class SnakeAndLadderGame {
 			  }
 			case LADDER:{
 				int playerPosition = person1.position;
-				playerPosition = playerPosition+100;
+				playerPosition = playerPosition+diceValue;
 				System.out.println("New position evaluated to "+playerPosition);
 				if(playerPosition>100)
 					System.out.println("You Stay Where You Are");
 				else
 					person1.position = playerPosition;
-				
+				rollingTheDie(person1);
 				break;
 			  }
 			case SNAKE:{
@@ -46,7 +44,25 @@ public class SnakeAndLadderGame {
 			
 			System.out.println("______________________");
 		}
-		System.out.println("Number of times the die wa srolled to win the game was: " + dieRollCount);
+		return dieRollCount;
+    }
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		Person person2 = new Person();
+		System.out.println("Rolling For Player1");
+		int person1DieRollCount = rollingTheDie(person1);
+		System.out.println("Rolling For Player2");
+		int person2DieRollCount = rollingTheDie(person2);
+		System.out.println("Rolling For Player1");
+		System.out.println("Player1 rolled "+ person1DieRollCount+ " times");
+		System.out.println("Player2 rolled "+ person2DieRollCount+ " times");
+		if (person1DieRollCount<person2DieRollCount) {
+			System.out.println("PLAYER1 WINS");
+		} else {
+			System.out.println("PLAYER2 WINS");
+		}
+		
+		
 		
 	}
 }
